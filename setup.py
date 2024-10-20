90da022923a710344e43ceba9745003d840366f1
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-20 12:29:53 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name="hugpi",
-    version="0.1.6",  # Incremented the version number
-    package_dir={"": "src"},
+    version="0.1.7",
     packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
-    ]
+        # List your core dependencies here
+        "numpy",
+        "pandas",
+    ],
+    extras_require={
+        "features": [
+            # List dependencies for the 'features' module here
+            "scikit-learn",
+            "matplotlib",
+        ],
+    },
 )
