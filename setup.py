a0995ace45c03a1cfa015cd1c79b68cd6c731241
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-20 12:46:04 +0530

    updated v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hugpi",
-    version="0.1.8",  # Incremented the version number
+    version="0.2.0",  # Incremented the version number
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
