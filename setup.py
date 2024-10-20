ebd269746c1da30b1525937b3fc1ce3af9f1fc6a
kammarisanthosh <kammari.santhosh@newgensoft.com>
2024-10-20 13:06:45 +0530

    removed api

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hugpi-features",
-    version="0.1.0",  # Incremented the version number
+    version="0.2.1",  # Incremented the version number
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=[
